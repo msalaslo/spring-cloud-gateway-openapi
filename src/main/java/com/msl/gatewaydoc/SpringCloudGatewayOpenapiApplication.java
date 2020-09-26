7e19c71f4a186c881e01e52fb5260fb92fd95c4b
msalas <msalas@vectoritcgroup.com>
2020-09-27 00:11:31 +0200

    Configuration for API versioned. Swagger console gatewayed. Loggers

--- src/main/java/com/msl/gatewaydoc/SpringCloudGatewayOpenapiApplication.java
+++ src/main/java/com/msl/gatewaydoc/SpringCloudGatewayOpenapiApplication.java
@@ -3,54 +3,35 @@ package com.msl.gatewaydoc;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.validation.constraints.NotNull;
-
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.context.annotation.Bean;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 @SpringBootApplication
 public class SpringCloudGatewayOpenapiApplication {
-
-    @NotNull
-    @Value("${server.servlet.context-path}")
-    private String contextPath;
-    
+     
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudGatewayOpenapiApplication.class, args);
 	}
 
 	@Autowired
 	RouteDefinitionLocator locator;
+	
+	private static final String SERVICE_TO_REGISTER_IN_SWAGGER_SUFIX = "-service";
 
 	@Bean
 	public List<GroupedOpenApi> apis() {
 		List<GroupedOpenApi> groups = new ArrayList<>();
 		List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
-		definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")).forEach(routeDefinition -> {
-			String name = routeDefinition.getId().replaceAll("-service", "");
+		definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*" + SERVICE_TO_REGISTER_IN_SWAGGER_SUFIX + "")).forEach(routeDefinition -> {
+			String name = routeDefinition.getId().replaceAll(SERVICE_TO_REGISTER_IN_SWAGGER_SUFIX, "");
 			groups.add(GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").setGroup(name).build());
 		});
 		return groups;
 	}
 	
-	
-	public List<GroupedOpenApi> apis1() {
-		List<GroupedOpenApi> groups = new ArrayList<>();
-		List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
-		definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")).forEach(routeDefinition -> {
-			String name = routeDefinition.getId().replaceAll("-service", "");
-			groups.add(GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").setGroup(name).addOpenApiCustomiser(new GatewayApi(contextPath, "")).build());
-		});
-		return groups;
-	}
-
 }
